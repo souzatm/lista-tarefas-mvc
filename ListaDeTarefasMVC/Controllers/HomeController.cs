0c68fcfe028c48f013a0c0284dc76b5ebe3a86cf
Thiago Souza <tsouzatm@outlook.com>
2024-06-19 15:11:43 -0300

    Adicionando Filtros na HomeController e View Index

--- ListaDeTarefasMVC/Controllers/HomeController.cs
+++ ListaDeTarefasMVC/Controllers/HomeController.cs
@@ -1,14 +1,65 @@
+using ListaDeTarefasMVC.Data;
 using ListaDeTarefasMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ListaDeTarefasMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
         {
-            return View();
+             _context = context;
+        }
+
+        public IActionResult Index(string id)
+        {
+            var filtros = new Filtros(id);
+
+            ViewBag.Filtros = filtros;
+            ViewBag.Categorias = _context.Categorias.ToList();
+            ViewBag.Status = _context.Statuses.ToList();
+            ViewBag.VencimentoValores = Filtros.VencimentoValoresFiltro;
+
+            IQueryable<Tarefa> consulta = _context.Tarefas
+                .Include(c => c.Categoria)
+                .Include(c => c.Status);
+
+            if (filtros.TemCategoria)
+            {
+                consulta = consulta.Where(t => t.CategoriaId == filtros.CategoriaId);
+            }
+
+            if (filtros.TemStatus)
+            {
+                consulta = consulta.Where(t => t.StatusId == filtros.StatusId);
+            }
+
+            if (filtros.TemVencimento)
+            {
+                var hoje = DateTime.Today;
+
+                if (filtros.EPassado)
+                {
+                    consulta = consulta.Where(t => t.DataDeVencimento < hoje);
+                }
+
+                if (filtros.EFuturo)
+                {
+                    consulta = consulta.Where(t => t.DataDeVencimento > hoje);
+                }
+
+                if (filtros.EHoje)
+                {
+                    consulta = consulta.Where(t => t.DataDeVencimento == hoje);
+                }
+            }
+
+            var tarefas = consulta.OrderBy(t => t.DataDeVencimento).ToList();
+
+            return View(tarefas);
         }
     }
 }
